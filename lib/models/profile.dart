100f2d0dd488bbe793d0928607668ee0b742c1b3
BuildTools <unconfigured@null.spigotmc.org>
2024-07-07 13:58:03 -0700

    Skills/Profile update Added more skills in class 'Skills' in profile.dart Formatted profile_page.dart differently

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -4,11 +4,27 @@ import 'skills.dart';
 class Skills {
   final Skill taming;
   final Skill farming;
+  final Skill mining; 
+  final Skill combat; 
+  final Skill foraging; 
+  final Skill fishing; 
+  final Skill enchanting; 
+  final Skill alchemy; 
+  final Skill carpentry; 
+  final Skill runecrafting; 
   // Add other skills here as needed
 
   Skills({
     required this.taming,
     required this.farming,
+    required this.mining,
+    required this.combat,
+    required this.foraging,
+    required this.fishing,
+    required this.enchanting, 
+    required this.alchemy,
+    required this.carpentry, 
+    required this.runecrafting,
     // Add other skills here as required
   });
 
@@ -16,10 +32,57 @@ class Skills {
     return Skills(
       taming: Skill.fromJson(json['taming'] ?? {}),
       farming: Skill.fromJson(json['farming'] ?? {}),
+      mining: Skill.fromJson(json['mining'] ?? {}),
+      combat: Skill.fromJson(json['combat'] ?? {}), 
+      foraging: Skill.fromJson(json['foraging'] ?? {}), 
+      fishing: Skill.fromJson(json['fishing'] ?? {}), 
+      enchanting: Skill.fromJson(json['enchanting'] ?? {}), 
+      alchemy: Skill.fromJson(json['alchemy'] ?? {}), 
+      carpentry: Skill.fromJson(json['carpentry'] ?? {}), 
+      runecrafting: Skill.fromJson(json['runecrafting'] ?? {}), 
       // Add other skills here as required
     );
   }
 }
+class Level{
+  final int level;
+  final int xpCurrent; 
+  final int xpForNext;
+
+Level({
+    required this.level, 
+    required this.xpCurrent,
+    required this.xpForNext,
+});
+
+
+
+factory Level.fromJson(Map<String, dynamic> json) {
+    return Level(
+      level: json['level'] ?? 0, 
+      xpCurrent: json['xpCurrent'] ?? 0, 
+      xpForNext: json['xpForNext'] ?? 0, 
+    );
+  }
+}
+
+
+class username{
+  final String name; 
+
+  username({
+    required this.name,
+  });
+
+
+  factory username.fromJson(Map<String, dynamic> json) {
+    return username(
+      name: json['display_name'] ?? '', 
+    );
+  }
+}
+
+
 
 class Profile {
   final String profileId;
@@ -27,6 +90,8 @@ class Profile {
   final String gameMode;
   final bool current;
   final Skills skills;
+  final Level levels;
+  final username name; 
 
   Profile({
     required this.profileId,
@@ -34,6 +99,8 @@ class Profile {
     required this.gameMode,
     required this.current,
     required this.skills,
+    required this.levels,
+    required this.name,
   });
 
   // Factory method to convert JSON into a Profile object
@@ -43,7 +110,9 @@ class Profile {
       cuteName: json['cute_name'] ?? '',
       gameMode: json['game_mode'] ?? '',
       current: json['current'] ?? false,
+      levels: Level.fromJson(json['data']['skyblock_level'] ?? {}),
       skills: Skills.fromJson(json['data']['skills']['skills'] ?? {}) ,  
+      name: username.fromJson(json['data'] ?? {}),
     );
   }
 }
